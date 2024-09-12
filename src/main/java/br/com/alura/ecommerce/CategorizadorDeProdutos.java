67027bc506310d5944c75beb8d4589590592f095
Lucas <rm550828@fiap.com.br>
2024-09-12 00:47:55 -0300

    upload project

---
+++ src/main/java/br/com/alura/ecommerce/CategorizadorDeProdutos.java
@@ -0,0 +1,59 @@
+package br.com.alura.ecommerce;
+
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CategorizadorDeProdutos {
+    public static void main(String[] args) {
+        var leitor = new Scanner(System.in);
+
+
+            System.out.println("Digite as categorias:");
+            var categorias = leitor.nextLine();
+
+        while (true) {
+            System.out.println("\nDigite o nome do produto:");
+            var user = leitor.nextLine();
+
+            var system = """
+                    Você é um categorizador de produtos e deve responder apenas o nome da categoria do produto informado pelo usuário.
+                    
+                    Escolha uma categoria dentro da lista abaixo:
+                    
+                    %s
+                    
+                    # Exemplo de uso:
+                    
+                    Pergunta: Bola de futebol
+                    Resposta: Esportes
+                    
+                    # Regras a serem seguidas:
+                    Caso o usuário pergunte algo que nao seja de categorizacao de produtos, voce deve responder que nao pode ajudar pois o seu papel é responder a categoria dos produtos.
+                    """.formatted(categorias);
+
+            dispararRequisicao(user, system);
+        }
+    }
+
+    public static void dispararRequisicao(String user, String system) {
+        var chave = System.getenv("OPENAI_API_KEY");
+        var service = new OpenAiService(chave, Duration.ofSeconds(30));
+        var completionRequest = ChatCompletionRequest
+                .builder()
+                .model("gpt-4")
+                .messages(Arrays.asList(
+                        new ChatMessage(ChatMessageRole.USER.value(), user),
+                        new ChatMessage(ChatMessageRole.SYSTEM.value(),system)
+                ))
+                .build();
+        service.createChatCompletion(completionRequest)
+                .getChoices()
+                .forEach(c -> System.out.print(c.getMessage().getContent()));
+    }
+}
